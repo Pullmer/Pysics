e02b482be5a1b9e642f758e21617533aac30cc91
Bruce <cyprienthomas@gmail.com>
2015-10-31 13:38:23 +0100

    ajout de classe
    pas de code pour l'instant, juste des specs

---
+++ Entity.py
@@ -0,0 +1,11 @@
+class Entity:
+	"""Classe abstraite définissant une entité, soit tout ce qui peut se trouver dans une scène
+	
+	Attributs :
+	- nom = nom de l'entité, affiché dans la liste des entité, ou sur l'entité elle même
+	- position = position de son origine/point d'application
+	
+	
+	"""
+	
+	
---
+++ PhysicalEntity.py
@@ -0,0 +1,10 @@
+class PhysicalEntity(Entity):
+	"""Classe abstraite définissant une entité physique, soit tout objet ayant une masse et des collisions
+	
+	Attributs :
+	- masse
+	
+	
+	"""
+	
+	
