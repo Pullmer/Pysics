b8913bb2288d52acbec2e1ab11c58b1e6eb47aa7
Bruce <cyprienthomas@gmail.com>
2015-11-15 22:49:35 +0100

    ajout

--- Entity.py
+++ Entity.py
@@ -5,6 +5,10 @@ class Entity:
 	- nom = nom de l'entité, affiché dans la liste des entité, ou sur l'entité elle même
 	- position = position de son origine/point d'application
 	
+	qsdfsfcsdfcsx
+	
+	
+	
 	
 	"""
 	
